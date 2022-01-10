475208f03997887cc5567312e35e1b39463f1dd6
yongchanson <syc9430@gmail.com>
2022-01-11 02:59:48 +0900

    landingPage

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
+import axios from 'axios';
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+    
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null) 
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+
+            console.log(response)
+
+            setMovies([...response.results])
+            setMainMovieImage(response.results[0])
+        })
+
+    }, [])
+    
+        
+    return (    
+        <div style={{ width: '100%', margin: '0'}}>
+
+            {/* main페이지 */}
+            {MainMovieImage &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+
+            
+            <div style={{ width: '85%', margin: '1rem auto'}}>
+                
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* movie grid cards */}
+
+
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button> Load More </button>
+            </div>
+        </div>
     )
 }
 
