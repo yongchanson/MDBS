c9c30bc3ea0d3c29a35be4bed6cd316577e84a30
yongchanson <syc9430@gmail.com>
2022-02-09 02:17:59 +0900

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -40,11 +40,11 @@ app.use("/uploads", express.static("uploads"));
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/bulid"));
+  app.use(express.static("client/publid"));
 
   // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "publid", "index.html"));
   });
 }
 
