b477882aa32ebecce009d5fc63f90fa4932763b8
yongchanson <syc9430@gmail.com>
2022-02-09 17:08:52 +0900

    make it better

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,10 +14,23 @@ import CastPage from "./views/Cast/CastPage";
 import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./views/commons/GlobalStyles";
 // import { lightTheme, darkTheme } from "./views/commons/Theme";
-import { lightTheme, darkTheme } from "./views/commons/Theme";
 
 import { Helmet } from "react-helmet";
 import Logo from "./views/commons/MDBS_LOGO.png";
+
+const lightTheme = {
+  body: "#FFF",
+  text: "#363537",
+  toggleBorder: "#363537",
+  toggleBody: "#001529",
+};
+
+const darkTheme = {
+  body: "#001529",
+  text: "#FFF",
+  toggleBorder: "#FFF",
+  toggleBody: "#001529",
+};
 
 const Wrapper = styled.div`
   paddingtop: "69px";
