529fa4623197eff3a1dfc2b53ab08736036a22a0
yongchanson <syc9430@gmail.com>
2022-02-10 16:05:46 +0900

    React Hook useEffect has a missing dependency: 'fetchMovies'. Either include it or remove the dependency array

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import MainImage from "../../views/commons/MainImage";
 import GridCards from "../commons/GridCards";
@@ -22,15 +22,18 @@ function LandingPage() {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=1`;
 
     fetchMovies(endpoint);
-  }, []);
+  }, [setCurrentPage]);
 
-  const fetchMovies = async (endpoint) => {
-    const response = await fetch(endpoint);
-    const response_1 = await response.json();
-    // console.log(response);
-    setMovies([...Movies, ...response_1.results]); //...Movies : 처음 출력되는 20개의 영화를 보존하기 위함(없을경우 기존 영화가 사라지고 새로운 영화가 나옴)
-    setMainMovieImage(response_1.results[0]);
-    setCurrentPage(response_1.page);
+  const fetchMovies = (endpoint) => {
+    return fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        // console.log(response);
+
+        setMovies([...Movies, ...response.results]); //...Movies : 처음 출력되는 20개의 영화를 보존하기 위함(없을경우 기존 영화가 사라지고 새로운 영화가 나옴)
+        setMainMovieImage(response.results[0]);
+        setCurrentPage(response.page);
+      });
   };
 
   function getMovies() {
