7ba719bc61141bc05ee16ff779049fac0bf1fb84
yongchanson <syc9430@gmail.com>
2022-02-19 22:48:32 +0900

    make it better

---
+++ app/build/precache-manifest.b43fb2ee59716fa106917f451ab54d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d828dbd8924e2f6f388ca42b00be61d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f0541cff103d66eebb6",
+    "url": "/static/css/2.9991e3dd.chunk.css"
+  },
+  {
+    "revision": "e861d84a35dc6a86a9d0",
+    "url": "/static/css/main.a623fdd3.chunk.css"
+  },
+  {
+    "revision": "0f0541cff103d66eebb6",
+    "url": "/static/js/2.ec0e9479.chunk.js"
+  },
+  {
+    "revision": "e861d84a35dc6a86a9d0",
+    "url": "/static/js/main.eedeafa6.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
