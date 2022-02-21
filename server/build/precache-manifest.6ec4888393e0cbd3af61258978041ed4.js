77002b0d44387aa6fb5f736a822d0f8bfb4e7c53
yongchanson <syc9430@gmail.com>
2022-02-21 19:32:57 +0900

    server/index.js

---
+++ server/build/precache-manifest.6ec4888393e0cbd3af61258978041ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a9ed0a3f7fac99e4fafe5eada4b533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02bd01e7bd923e2dc5d2",
+    "url": "/static/css/2.9991e3dd.chunk.css"
+  },
+  {
+    "revision": "bf584ae65e0824b90377",
+    "url": "/static/css/main.a623fdd3.chunk.css"
+  },
+  {
+    "revision": "02bd01e7bd923e2dc5d2",
+    "url": "/static/js/2.8bac454f.chunk.js"
+  },
+  {
+    "revision": "bf584ae65e0824b90377",
+    "url": "/static/js/main.1e71013b.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
